4ebcba28da103752b2517ed66e9f35276d0ee6ef
Joshua Devon <joshua.devon@gmail.com>
2024-04-23 13:11:56 +1000

    Logic for comparing thrown hands

--- script.js
+++ script.js
@@ -6,24 +6,30 @@ const compThrow = document.getElementById("comp_button");
 
 const result = document.getElementById("result");
 
-let selected = "rock";
+let userChoice = "rock";
+let computerChoice = getComputerChoice();
+
+let humanScore = 0;
+let computerScore = 0;
+
+
 
 $(rockImage).on('click', function() {
     $(".images img").removeClass("selected");
     $(this).addClass("selected");
-    selected = "rock";
+    userChoice = "rock";
 });
 
 $(paperImage).on('click', function() {
     $(".images img").removeClass("selected");
     $(this).addClass("selected");
-    selected = "paper";
+    userChoice = "paper";
 });
 
 $(scissorsImage).on('click', function() {
     $(".images img").removeClass("selected");
     $(this).addClass("selected");
-    selected = "scissors";
+    userChoice = "scissors";
 });
 
 function getComputerChoice() {
@@ -32,5 +38,25 @@ function getComputerChoice() {
         case 0: return "rock";
         case 1: return "paper";
         case 2: return "scissors";
+    }
+}
+
+// 0 = Draw, -1 = Loss, 1 = Win
+function compareResult(humanChoice, computerChoice) {
+    let result = 0;
+    if (humanChoice === computerChoice) {
+        result = 0;
+    } else if (humanChoice === "rock" && computer === "paper") {
+        result = 1;
+    } else if (humanChoice === "rock" && computer === "scissors") {
+        result = -1;
+    } else if (humanChoice === "paper" && computer === "rock") {
+        result = 1;
+    } else if (humanChoice === "paper" && computer === "scissors") {
+        result = -1;
+    } else if (humanChoice === "scissors" && computer === "rock") {
+        result = -1;
+    } else if (humanChoice === "scissors" && computer === "paper") {
+        result = 1;
     }
 }
